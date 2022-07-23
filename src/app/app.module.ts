b52205ee97fa53fcd19438ab467dfb8acab3c68a
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-23 19:12:10 +0200

    users and hosts Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './Users-Modules/users/users.component';
+import { HostComponent } from './Host-Modules/host/host.component';
+import { HostsComponent } from './Hosts-Modules/hosts/hosts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HostComponent,
+    HostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
