9d79eb919b81246ea6b1277c0be77e31616de757
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-23 20:44:35 +0200

    new update header and hostes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './Header-Module/header/header.component';
+import { HostsComponent } from './Hosts-Module/hosts/hosts.component';
+import { UsersComponent } from './Users-Modules/users/users.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'user',component:UsersComponent},
+  {path:'host',component:HostsComponent},
+  {path:'header',component:HeaderComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsersComponent } from './Users-Modules/users/users.component';
-import { HostComponent } from './Host-Modules/host/host.component';
-import { HostsComponent } from './Hosts-Modules/hosts/hosts.component';
+import { HostsComponent } from './Hosts-Module/hosts/hosts.component';
+import { HeaderComponent } from './Header-Module/header/header.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    HostComponent,
-    HostsComponent
+    HostsComponent,
+    HeaderComponent,
+ 
   ],
   imports: [
     BrowserModule,
