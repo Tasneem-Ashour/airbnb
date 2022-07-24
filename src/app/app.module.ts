73f080b46efc1a0b0f11512dd799e5ef2f7dd3c1
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-24 12:59:45 +0200

    last update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UsersComponent } from './Users-Modules/users/users.component';
-import { HostsComponent } from './Hosts-Module/hosts/hosts.component';
-import { HeaderComponent } from './Header-Module/header/header.component';
+import {ButtonModule} from 'primeng/button';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                  
+
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    HostsComponent,
-    HeaderComponent,
+    
+ 
  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
