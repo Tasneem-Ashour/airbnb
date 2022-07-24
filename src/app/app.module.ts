29346ee87f739fa3b003612b0f82f5d67470afbe
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-24 13:25:16 +0200

    json update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ButtonModule} from 'primeng/button';
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
-import {MenuItem} from 'primeng/api';                  
+                
 
 
 
