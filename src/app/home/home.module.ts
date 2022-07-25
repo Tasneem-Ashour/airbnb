79fa70523500b8332f709227df32dd16a0f6a722
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-25 21:38:41 +0200

    signin

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SignInPageComponent, SignUpPageComponent, HomePageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   exports: [SignInPageComponent, SignUpPageComponent, HomePageComponent],
 })
 export class HomeModule {}
--- src/app/home/sign-in-page/sign-in-page.component.ts
+++ src/app/home/sign-in-page/sign-in-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
   styleUrls: ['./sign-in-page.component.css']
 })
-export class SignInPageComponent implements OnInit {
+export class SignInPageComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  form=new FormGroup({
+    email: new FormControl('', [Validators.required , Validators.email]  ),
+    password: new FormControl('', [Validators.minLength(8) , Validators.required ]) 
+  })
+   
+  get email(){
+    return this.form.get("email");
+  }
+  
+  get password(){
+    return this.form.get("password");
+  }
+  
   }
 
-}
+
