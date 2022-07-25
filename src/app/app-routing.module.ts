172386e4eda1bfcb61ed3610022e918ace98d351
MohamedAymanZakria <106837274+MohamedAymanZakria@users.noreply.github.com>
2022-07-25 16:06:43 +0200

    signIn&signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInPageComponent } from './home/sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './home/sign-up-page/sign-up-page.component';
 
-const routes: Routes = [{ path: "signup", component: SignUpPageComponent }];
+const routes: Routes = [
+  { path: "signup", component: SignUpPageComponent },
+  { path: "signin", component: SignInPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
