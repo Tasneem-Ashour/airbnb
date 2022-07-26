492d6300f3523ff17e1abc57356421440950be71
MohamedAymanZakria <106837274+MohamedAymanZakria@users.noreply.github.com>
2022-07-26 11:56:15 +0200

    vFirst

--- src/app/home/sign-up-page/sign-up-page.component.ts
+++ src/app/home/sign-up-page/sign-up-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-page',
   templateUrl: './sign-up-page.component.html',
   styleUrls: ['./sign-up-page.component.css']
 })
-export class SignUpPageComponent implements OnInit {
+export class SignUpPageComponent  {
 
-  constructor() { }
+  upForm=new FormGroup({firstName:new FormControl('',[Validators.required,Validators.maxLength(8)])
 
-  ngOnInit(): void {
+  })
+
+  get firstName(){
+    return this.upForm.get("firstName");
   }
+
 
 }
