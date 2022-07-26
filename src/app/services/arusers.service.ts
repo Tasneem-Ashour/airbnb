e0f813328f1919234bf34fc4632f4e836ebbc049
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-26 18:29:56 +0200

    api 1

--- src/app/services/arusers.service.ts
+++ src/app/services/arusers.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Arusers } from '../_models/arusers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArusersService {
 
-  constructor() { }
+  userLogin(){
+    this.http.post<Arusers[]>("https://localhost:7038/api/arusers/login" , '').subscribe(a=>{console.log(a)})
+
+  }
+  constructor(public http:HttpClient) { }
 }
