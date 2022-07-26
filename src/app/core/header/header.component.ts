ca71a6d658d3b9d3894dc6993c68199deae3b990
MohamedAymanZakria <106837274+MohamedAymanZakria@users.noreply.github.com>
2022-07-26 18:07:25 +0200

    vSignUp&header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  mainNav = true;
+  @Output() clickEvent = new EventEmitter<boolean>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ShowNewNav() {
+    this.mainNav = false;
+    this.clickEvent.emit(this.mainNav);
   }
-
 }
--- src/app/home/sign-up-page/sign-up-page.component.ts
+++ src/app/home/sign-up-page/sign-up-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { PassValidService } from 'src/app/services/pass-valid.service';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -8,12 +10,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SignUpPageComponent  {
 
-  upForm=new FormGroup({firstName:new FormControl('',[Validators.required,Validators.maxLength(8)])
+  upForm=new FormGroup({
+  firstName:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+  lastName:new FormControl('',[Validators.required,Validators.minLength(4)]),
+  email:new FormControl('',[Validators.required,Validators.email]),
+  password: new FormControl('', [Validators.required]),
+  confirmPassword: new FormControl('', [Validators.required]),
 
-  })
+  },[PassValidService.MatchValidator('password', 'confirmPassword')])
 
   get firstName(){
     return this.upForm.get("firstName");
+  }
+  get lastName(){
+    return this.upForm.get("lastName")
+  }
+  get email(){
+    return this.upForm.get("email")
+  }
+  get passwordMatchError() {
+    return (
+      this.upForm.getError('mismatch') &&
+      this.upForm.get('confirmPassword')?.touched
+    );
   }
 
 
