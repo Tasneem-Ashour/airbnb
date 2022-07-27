95c3263b81aaf440802947bf283625bef9ccc43e
MohamedAymanZakria <106837274+MohamedAymanZakria@users.noreply.github.com>
2022-07-27 15:17:29 +0200

    edituserprofile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { SignInPageComponent } from './home/sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './home/sign-up-page/sign-up-page.component';
+import { UserEditProfileComponent } from './home/user-edit-profile/user-edit-profile.component';
+import { UserProfileComponent } from './home/user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: "signup", component: SignUpPageComponent },
   { path: "signin", component: SignInPageComponent },
   { path: "home", component: HomePageComponent },
+  { path: "profile", component: UserProfileComponent },
+  { path: "edit", component: UserEditProfileComponent },
+
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,19 @@ import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
+
 import {MessagesModule} from 'primeng/messages';
 import {MessageModule} from 'primeng/message';
 import {ButtonModule} from 'primeng/button';
 import { RouterModule } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserEditProfileComponent } from './user-edit-profile/user-edit-profile.component';
 
 
 @NgModule({
-  declarations: [SignInPageComponent, SignUpPageComponent, HomePageComponent],
+  declarations: [SignInPageComponent, SignUpPageComponent, HomePageComponent, UserProfileComponent, UserEditProfileComponent],
   imports: [CommonModule,ReactiveFormsModule,MessagesModule,MessageModule,ButtonModule,RouterModule],
-  exports: [SignInPageComponent, SignUpPageComponent, HomePageComponent],
+  exports: [SignInPageComponent, SignUpPageComponent, HomePageComponent,UserProfileComponent,UserEditProfileComponent],
 })
 export class HomeModule {}
