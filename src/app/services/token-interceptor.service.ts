44073d75cf8e67a238c5ee6da4c52ed195d427b0
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-29 00:04:39 +0200

    get data of user

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -11,10 +11,12 @@ export class TokenInterceptorService implements HttpInterceptor{
   constructor(private injector:Injector) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authService= this.injector.get(UserAuthService);
+    var token =localStorage.getItem('token')
     let tokenRequset = req.clone({
-      setHeaders:{
-        Authrization:`Bearer ${authService.getToken()}`
-      }
+      // setHeaders:{
+      //   Authrization:`Bearer ${authService.getToken()}`
+      // }
+      headers:req.headers.set('Authorization' ,`Bearer ${token}`)
     })
     return next.handle(tokenRequset);
   }
