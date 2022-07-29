0a575efad4866a0a3f2ccb7f9e97b1b0e643f237
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-30 01:25:22 +0200

    category , subcategory , reviews

---
+++ src/app/_models/proprty-reviwes.ts
@@ -0,0 +1,14 @@
+export class ProprtyReviwes {
+    constructor(
+
+        id: number,
+        propertyId: number,
+        reviewByUser: string,
+        bookingId: number,
+        comment: string,
+        rating: number,
+        created: Date,
+        modified: Date,
+        status: boolean
+    ){}
+}
