e6b8e350e8dfb9884d8ce22a0239caf9ad6c983c
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-07-31 18:18:24 +0200

    room crud

--- src/app/services/host.service.ts
+++ src/app/services/host.service.ts
@@ -41,29 +41,63 @@ deleteProperty(deleteProprty:HostProperties){
 // category crud
 
 getAllCategory():Observable<Category[]>{
-  return this.http.get<Category[]>(`${environment.APIURL}/ArCategories`)
+  return this.http.get<Category[]>(`${environment.APIURL}/ArCategories`);
 }
 
 AddCategory(AddCategory:Category){
-  return this.http.post<Category>(`${environment.APIURL}/ArCategories/${AddCategory.id}`,AddCategory)
+  return this.http.post<Category>(`${environment.APIURL}/ArCategories/${AddCategory.id}`,AddCategory);
 }
 
 updateCategory(updateCategory:Category){
-  return this.http.put<Category>(`${environment.APIURL}/ArCategories/${updateCategory.id}`,updateCategory)
+  return this.http.put<Category>(`${environment.APIURL}/ArCategories/${updateCategory.id}`,updateCategory);
 }
 
 
 deleleCategory(deleleCategory:Category){
-  return this.http.delete<Category>(`${environment.APIURL}/ArCategories/${deleleCategory.id}`)
+  return this.http.put<Category>(`${environment.APIURL}/ArCategories/${deleleCategory.id}`,deleleCategory);
 }
 
 
 // sub Category crud
 
 getAllSubCategory():Observable<SubCategory[]>{
-  return this.http.get<SubCategory[]>(`${environment.APIURL}/ArSubcategories`)
+  return this.http.get<SubCategory[]>(`${environment.APIURL}/ArSubcategories`);
 }
   
+AddSubCategory(AddSubCategory:SubCategory){
+return this.http.post<SubCategory>(`${environment.APIURL}/ArSubcategories`,AddSubCategory);
+}
+
+updateSubCategory(updateSubCategory:SubCategory){
+  return this.http.put<SubCategory>(`${environment.APIURL}/ArSubcategories/${updateSubCategory.id}`,updateSubCategory);
+}
+deleteSubCategory(deleleSubCategory:SubCategory){
+return this.http.put<SubCategory>(`${environment.APIURL}/ArSubcategories/${deleleSubCategory.id}`,deleleSubCategory);
+}
+
+
+//room crud 
+
+getRoomType():Observable<RoomType[]>{
+  return this.http.get<RoomType[]>(`${environment.APIURL}/ArRoomTypes`);
+}
+
+
+AddRoom(AddRoom:RoomType){
+  return this.http.post<RoomType>(`${environment.APIURL}/ArRoomTypes`,AddRoom);
+}
+
+//only for admin 
+
+updateRoom(updateRoom:RoomType){
+  return this.http.put<RoomType>(`${environment.APIURL}/ArRoomTypes${updateRoom.id}`,updateRoom);
+}
+
+//only for admin
+deleteRoom(deleteRoom:RoomType){
+  return this.http.delete<RoomType>(`${environment.APIURL}/ArRoomTypes${deleteRoom.id}`);
+}
+
 getContries():Observable<Countries[]>{
   return this.http.get<Countries[]>(`${environment.APIURL}/ArCountries/GetAll`)
 }
@@ -72,9 +106,6 @@ getCities():Observable<Cities[]>{
   return this.http.get<Cities[]>(`${environment.APIURL}/ArCities/GetAll`)
 }
 
-getRoomType():Observable<RoomType[]>{
-  return this.http.get<RoomType[]>(`${environment.APIURL}/ArRoomTypes`)
-}
 
 
 }
