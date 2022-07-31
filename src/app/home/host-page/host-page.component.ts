e14ceb3486f2aff7b377826de7015a51ec6f4eb5
MohamedAymanZakria <106837274+MohamedAymanZakria@users.noreply.github.com>
2022-07-31 05:29:25 +0200

    host

--- src/app/home/host-page/host-page.component.ts
+++ src/app/home/host-page/host-page.component.ts
@@ -7,99 +7,75 @@ import { HostProperties } from 'src/app/_models/host-properties';
 import { RoomType } from 'src/app/_models/room-type';
 import { SubCategory } from 'src/app/_models/sub-category';
 
-
-
 @Component({
   selector: 'app-host-page',
   templateUrl: './host-page.component.html',
-  styleUrls: ['./host-page.component.css']
+  styleUrls: ['./host-page.component.css'],
 })
+export class HostPageComponent {
+  constructor(
+    public allCountries: HostService,
+    public allCities: HostService,
+    public roomType: HostService,
+    public hostProperty: HostService,
+    public category: HostService,
+    public subCategory: HostService
+  ) {}
+  Countries: Countries[] = [];
+  Cities: Cities[] = [];
+  Room: RoomType[] = [];
+  hostProp: HostProperties[] = [];
+  cat: Category[] = [];
+  sub: SubCategory[] = [];
 
+  selectedCountry = '';
+  selectedCity = '';
 
-export class HostPageComponent  {
+  ngOnInit(): void {}
 
-constructor(public allCountries:HostService ,
-  public allCities:HostService ,
-  public roomType:HostService,
-  public hostProperty:HostService,
-  public category:HostService,
-  public subCategory:HostService
-  
-  ){}
-Countries: Countries[]=[];  
-Cities:Cities[]=[];
-Room:RoomType[]=[];
-hostProp:HostProperties[]=[];
-cat:Category[]=[];
-sub:SubCategory[]=[];
-
-
-
-selectedCountry='';
-selectedCity='';
-
-  ngOnInit(): void {
-  }
-
-  scroll(e1:HTMLElement){
+  scroll(e1: HTMLElement) {
     e1.scrollIntoView();
   }
 
+  saveData() {}
 
-
-  saveData(){}
-
-
-  getAllPropirty(){
-    this.hostProperty.getAllProperties().subscribe(
-      (req)=>{
-        this.hostProp=req
-      }
-    );
-
+  getAllPropirty() {
+    this.hostProperty.getAllProperties().subscribe((req) => {
+      this.hostProp = req;
+    });
   }
 
-  getAllCategories(){
-    this.category.getAllCategory().subscribe(
-      (req)=>{
-        this.cat=req
-      }
-    );
-
+  getAllCategories() {
+    this.category.getAllCategory().subscribe((req) => {
+      this.cat = req;
+    });
   }
 
-
-  getAllSubCategories(){
-    this.subCategory.getAllSubCategory().subscribe(
-      (req)=>{
-        this.sub=req
-      }
-    );
-
+  getAllSubCategories() {
+    this.subCategory.getAllSubCategory().subscribe((req) => {
+      this.sub = req;
+    });
   }
 
-
-
-
-  getAllCountries(){
-    this.allCountries.getContries().subscribe(
-      (country)=>{
-        this.Countries=country
-      }
-    );
+  getAllCountries() {
+    this.allCountries.getContries().subscribe((country) => {
+      this.Countries = country;
+    });
   }
 
-  getAllCities(){
-    this.allCities.getCities().subscribe(
-      (city)=>{this.Cities=city}
-    );
+  getAllCities() {
+    this.allCities.getCities().subscribe((city) => {
+      this.Cities = city;
+    });
   }
 
-  getRoomType(){
-this.roomType.getRoomType().subscribe(
-  (req)=>{this.Room=req}
-);
+  getRoomType() {
+    this.roomType.getRoomType().subscribe((req) => {
+      this.Room = req;
+    });
   }
-  
-
+  //change acvtive
+  placeActive(event: any) {
+    document.getElementById('current')?.classList.remove('active');
+  }
 }
