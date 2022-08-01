f0ea652a8dca9df1c28a0259da0f2151ebe82d3c
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-08-01 13:02:11 +0200

    get host property by id

--- src/app/services/host.service.ts
+++ src/app/services/host.service.ts
@@ -26,6 +26,9 @@ getAllProperties():Observable<HostProperties[]>{
   return this.http.get<HostProperties[]>(`${environment.APIURL}/ArCategories`)
 }
 
+getPropeetiesByHostId(HostID:HostProperties):Observable<HostProperties[]>{
+  return this.http.get<HostProperties[]>(`${environment.APIURL}/ArProperties/GetWithAllData/${HostID.id}`);
+}
 AddProperty(AddProprty:HostProperties){
   return this.http.post<HostProperties>(`${environment.APIURL}/ArProperties`,AddProprty);
 }
